654773828439c95b6b99911a8f06bc79cbf05a8d
akshayst369 <akshayst369@gmail.com>
2026-04-19 11:50:31 +0530

    leetcode Brute force

---
+++ twosum-bruteforce.java
@@ -0,0 +1,20 @@
+//leetcode 1.TwoSum
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                if(nums[i]+nums[j]==target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
